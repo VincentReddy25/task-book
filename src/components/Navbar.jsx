0f9eb072ac9eaab71f969b92ac9ac72f36cc0653
vincent reddy <vincentreddy25@gmail.com>
2025-10-19 13:26:17 +0530

    feat(navbar): implemented basic navbar layout for routing feat(router): add react-router-dom for navigation feat(components): created Dashboard, Profile and Navbar files in components folder

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import { Link, Routes, Route } from 'react-router-dom';
+import Dashboard from './Dashboard';
+
+export default function Navbar() {
+    return (
+        <>
+            <div className="navbar">
+                <Link to='/dashboard'>Dashboard</Link>
+                <Link to='/profile'>Profile</Link>
+                <Link to='/logout'>Logout</Link>
+            </div>
+
+            <Routes>
+                <Route path='/' element={<Dashboard />} />
+                <Route path='/dashboard' element={<Dashboard />} />
+                <Route path='/profile' element={<Profile />} />
+            </Routes>
+        </>
+    );
+}
