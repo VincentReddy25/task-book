e4c788c0465ad4394bcfeb534e89234f434359b5
vincent reddy <vincentreddy25@gmail.com>
2025-10-19 14:02:09 +0530

    feat(form): add form and handler function

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,35 @@
+import { useState } from "react"
 
 export default function Dashboard() {
+
+  
+  const [taskData, setTaskData] = useState([])
+  
+  const addTask = (e) => {
+    event.preventDefault()
+    
+    let task = e.target.userInput
+    
+    setTaskData(prev => [...prev, task.value])
+
+  }
+
+
   return (
-    <div>Dashboard</div>
+    <>
+      <div>Dashboard</div>
+
+      <form action="" onSubmit={addTask}>
+        <input type="text" name="userInput" />
+        <input type="submit" />
+      </form>
+
+
+      <ul>
+        {taskData.map((key, index) => (
+          <li>{key}</li>
+        ))}
+      </ul>
+    </>
   )
 }
